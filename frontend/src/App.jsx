134fedcf1957b46dba0b84c429d693bc1c2e7296
Melnikov Kirill <kirillmelnikov2078@gmail.com>
2026-03-21 17:28:47 +0300

    Добавление условного отображения

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,35 @@
+import { useEffect, useState } from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Counter from './Counter';
 
 function App() {
+  const [showFooter, setShowFooter] = useState(true);
+
+  useEffect(() => {
+    alert('Привет!');
+  }, []);
+
+  const toggleFooter = () => {
+    setShowFooter(!showFooter);
+  };
+
   return (
     <div>
       <Header
         title="Мой React проект"
         subtitle="Мой первый React проект"
       />
+
       <Counter />
 
-      <Footer
-        text="Мой сайт."
-      />
+      <button onClick={toggleFooter}>
+        {showFooter ? 'Скрыть Footer' : 'Показать Footer'}
+      </button>
+
+      {showFooter && (
+        <Footer text="Мой сайт." />
+      )}
     </div>
   );
 }
